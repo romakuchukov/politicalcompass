172285bb01fc7a23fa01039536964a462fd5f6f7
Roma K <romankuchukov@gmail.com>
2020-09-08 14:20:49 -0400

    moved onwarn outside of export default

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,13 @@
 import node from 'rollup-plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
 
+const onwarn = (warning, warn) => {
+  if (warning.code === 'CIRCULAR_DEPENDENCY') return;
+  warn(warning);
+};
+
 export default [{
-  onwarn: function (warning, warn) {
-    if (warning.code === 'CIRCULAR_DEPENDENCY') return;
-    warn(warning);
-  },
+  onwarn: onwarn,
   input: 'src/index.js',
   output: {
     name: 'd3',
