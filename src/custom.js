a4f8bb13647fe6f7161a14f3ab1319ed96b9426a
Roma K <romankuchukov@gmail.com>
2020-09-10 01:26:59 -0400

    finished graph

--- src/custom.js
+++ src/custom.js
@@ -1,1 +1,117 @@
-d3.select("body").append("h1").text("Hello, world!");
+import data from './data'
+
+// d3.csv('./aapl.csv').then(data => {
+
+
+// });
+
+function formatValue(value) {
+  return value.toLocaleString('en', {
+      style: 'currency',
+      currency: 'USD'
+  });
+}
+
+function formatDate(date) {
+  return date.toLocaleString('en', {
+      month: 'short',
+      day: 'numeric',
+      year: 'numeric',
+      timeZone: 'UTC'
+  });
+}
+
+const height = 500;
+const width = 1000;
+const margin = { top: 20, right: 30, bottom: 30, left: 40 };
+const x = d3.scaleUtc().domain(d3.extent(data, d => new Date(d.date))).range([margin.left, width - margin.right]);
+const y = d3.scaleLinear().domain([0, d3.max(data, d => +d.value)]).nice().range([height - margin.bottom, margin.top])
+
+const callout = (g, value) => {
+  if (!value) return g.style('display', 'none');
+
+  g.style('display', null).style('pointer-events', 'none').style('font', '10px sans-serif');
+
+  const path = g.selectAll('path')
+      .data([null])
+      .join('path')
+      .attr('fill', 'white')
+      .attr('stroke', 'black');
+
+  const text = g.selectAll('text')
+      .data([null])
+      .join('text')
+      .call(text => text
+          .selectAll('tspan')
+          .data((value + '').split(/\n/))
+          .join('tspan')
+          .attr('x', 0)
+          .attr('y', (d, i) => `${i * 1.1}em`)
+          .style('font-weight', (_, i) => i ? null : 'bold')
+          .text(d => d));
+
+  const { x, y, width: w, height: h } = text.node().getBBox();
+
+  text.attr('transform', `translate(${-w / 2},${15 - y})`);
+  path.attr('d', `M${-w / 2 - 10},5H-5l5,-5l5,5H${w / 2 + 10}v${h + 20}h-${w + 20}z`);
+}
+const xAxis = g => g
+    .attr('transform', `translate(0,${height - margin.bottom})`)
+    .call(d3.axisBottom(x).ticks(width / 80).tickSizeOuter(0))
+
+const yAxis = g => g
+    .attr('transform', `translate(${margin.left},0)`)
+    .call(d3.axisLeft(y))
+    .call(g => g.select('.domain').remove())
+    .call(g => g.select('.tick:last-of-type text')
+    .clone()
+    .attr('x', 3)
+    .attr('text-anchor', 'start')
+    .attr('font-weight', 'bold')
+    .text('$ Close'))
+
+const line = d3.line().curve(d3.curveStep).defined(d => !isNaN(d.value)).x(d => x(new Date(d.date))).y(d => y(d.value));
+
+const bisect = mx => {
+  const date = x.invert(mx);
+  const index = d3.bisector(d => new Date(d.date)).left(data, date, 1);
+  const a = data[index - 1];
+  const b = data[index];
+  return b && (date - new Date(a.date) > new Date(b.date) - date) ? b : a;
+}
+
+function chart (d3, width, height, xAxis, yAxis, data, line, bisect, x, y, callout, formatValue, formatDate) {
+
+  const svg = d3.select('body')
+      .append('svg')
+      .style('-webkit-tap-highlight-color', 'transparent')
+      .style('overflow', 'visible')
+      .style('height', '100%')
+      .style('width', '100%')
+
+  svg.append('g').call(xAxis);
+
+  svg.append('g').call(yAxis);
+
+  svg.append('path')
+      .datum(data)
+      .attr('fill', 'none')
+      .attr('stroke', 'steelblue')
+      .attr('stroke-width', 1.5)
+      .attr('stroke-linejoin', 'round')
+      .attr('stroke-linecap', 'round')
+      .attr('d', line);
+
+  const tooltip = svg.append('g');
+
+  svg.on('touchmove mousemove', function (event) {
+      const { date, value } = bisect(d3.pointer(event, this)[0]);
+      tooltip.attr('transform', `translate(${x(new Date(date))},${y(value)})`).call(callout, `${formatValue(value)} ${formatDate(new Date(date))}`);
+  });
+
+  svg.on('touchend mouseleave', () => tooltip.call(callout, null));
+
+  return svg.node();
+}
+
+chart(d3, width, height, xAxis, yAxis, data, line, bisect, x, y, callout, formatValue, formatDate);
--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-export { select, selectAll } from 'd3-selection';
+// export { csv } from 'd3-fetch';
+// export { csvParse } from 'd3-dsv';
+
+export { line, curveStep } from 'd3-shape';
+export { axisBottom, axisLeft  } from 'd3-axis';
+export { extent, max, bisector } from 'd3-array';
+export { scaleUtc, scaleLinear } from 'd3-scale';
+export { select, selectAll, selection, pointer } from 'd3-selection';
