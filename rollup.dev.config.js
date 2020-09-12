f3a84a07d402ed729d02ef97884b90e54637b9f1
Roma K <romankuchukov@gmail.com>
2020-09-12 10:24:36 -0400

    child_process

--- rollup.dev.config.js
+++ rollup.dev.config.js
@@ -1,6 +1,15 @@
 import configs from './rollup.config';
-import browsersync from 'rollup-plugin-browsersync';
 
-browsersync();
+// const { exec } = require('child_process');
+// exec('browser-sync --serveStatic . --files dist -w', (err, stdout, stderr) => {
+//   if (err) {
+//     console.log(err)
+//     return;
+//   }
+
+//   // the *entire* stdout and stderr (buffered)
+//   console.log(`stdout: ${stdout}`);
+//   console.log(`stderr: ${stderr}`);
+// });
 
 export default configs;
