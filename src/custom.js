cdcbe8b718a421bc9950f3132e8e85b254133cdc
Roma K <romankuchukov@gmail.com>
2020-09-13 18:51:15 -0400

    rescaled width

--- src/custom.js
+++ src/custom.js
@@ -87,11 +87,11 @@ const svg = d3.select('body')
   .classed('svg', true)
   .append('svg')
   .attr('preserveAspectRatio', 'xMinYMin meet')
-  .attr('viewBox', '0 0 400 740')
+  .attr('viewBox', '0 0 400 490')
   .classed('responsive', true)
   .style('-webkit-tap-highlight-color', 'transparent')
   .style('overflow', 'visible')
-  .style('height', '150%')
+  .style('height', '100%')
   .style('width', '100%');
 
 svg.append('g').call(xAxis);
