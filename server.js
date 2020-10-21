1cb17adab677ea78f4d7f640e012527511246615
Roma K <romankuchukov@gmail.com>
2020-10-20 21:18:46 -0400

    fixing

---
+++ server.js
@@ -0,0 +1,25 @@
+//server.js
+const express = require('express');
+const favicon = require('express-favicon');
+const path = require('path');
+const port = process.env.PORT || 8080;
+const app = express();
+
+if(process.env.NODE_ENV === 'production') {
+  app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https')
+      res.redirect(`https://${req.header('host')}${req.url}`)
+    else
+      next()
+  })
+}
+
+app.use(favicon(__dirname + '/favicon.ico'));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname)));
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+app.listen(port);
